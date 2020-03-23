484814acd33a9ad45ea12ca40c216014b82d2e93
VictorKmin <kminvictor@gmail.com>
2020-03-23 22:31:31 +0200

    lesson1

---
+++ lesson1/scrtpt.js
@@ -0,0 +1,99 @@
+// let name = 'Viktor';
+// let age = 24.5;
+// let wife = false;
+
+// let isConfirm = confirm('Are u 18?');
+
+// document.write(isConfirm);
+//
+// console.log(isConfirm);
+// console.log(22);
+// console.log('TEST');
+
+
+// let xxx = prompt('ПРИВІТ ЧАТІК'); // ЧЕКАЮ
+// console.log(xxx);
+// document.write(xxx);
+// document.write('_____________');
+// document.write(name);
+
+// let number1 = prompt('ENTER NUMBER 1'); //10
+// let number2 = prompt('ENTER NUMBER 2'); // 888
+// let number1 = 10;
+// let number2 = 888;
+
+// let age = prompt('Введіть свій вік');
+// > < <= >= == != === !==
+// if (age === 18) {
+//     document.write('<b>Welcome</b>');
+// } else {
+//     document.write('<b>Smth went wrong</b>');
+// }
+//
+// document.write('<h1> 22 !!! </h1>');
+
+
+// let age = prompt('Enter age');
+// let name = prompt('Enter name');
+
+//
+// if (age >= 18) {
+//     document.write('<b>Welcome</b>');
+// } else {
+//
+//     if (name === 'admin') {
+//         document.write('<b>Welcome</b>');
+//     } else {
+//         document.write('<b>TOO YOUNG !!! BOOOY</b>');
+//     }
+// }
+
+// || => АБО
+// && => І
+// if (age >= 18 || name === 'admin') {
+//     document.write('<b>Welcome</b>');
+// } else {
+//     document.write('<b>TOO YOUNG !!! BOOOY</b>');
+// }
+
+// if (age >= 18 && age <= 60) {
+//     document.write('<b>Welcome</b>');
+// } else if (age > 60) {
+//     document.write('<b>TOO old, дідо</b>');
+// } else {
+//     document.write('<b>TOO YOUNG !!! BOOOY</b>');
+// }
+
+// let name = prompt('Enter name');
+// switch (name) {
+//     case 'Viktor':
+//         console.log('Привіт дядя Вітя');
+//         break;
+//     case 'Karina':
+//         console.log('Хелоу дарлінг');
+//         break;
+//     case 'Dasha':
+//         console.log('Првітулі');
+//         break;
+//     default:
+//         console.log('Шось не так');
+// }
+//
+
+
+// let name = prompt('Enter smth');
+// document.write(name);
+//
+// name = prompt('Enter again');
+// document.write('<br>');
+//
+// document.write(name);
+
+let name = prompt('Enter smth');
+
+if (name ==='admin') {
+    let pass = prompt('Enter pass');
+
+    console.log(pass);
+}
+
